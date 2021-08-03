2afe5419beb1b070820d893bb40a74915d3b9394
ChenJinBIT <46422215+ChenJinBIT@users.noreply.github.com>
2021-08-03 19:33:39 +0800

    update load networks

--- models/base_model.py
+++ models/base_model.py
@@ -189,6 +189,7 @@ class BaseModel(ABC):
                 state_dict.pop('.'.join(keys))
         else:
             self.__patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
+    '''
     def load_networks(self, epoch):
         """Load all the networks from the disk.
 
@@ -221,7 +222,7 @@ class BaseModel(ABC):
             epoch (int) -- current epoch; used in the file name '%s_net_%s.pth' % (epoch, name)
         """
         for name in self.model_names:
-            if name=='D_A' or name=='D_B':
+            if name=='D_A'or name=='D_B' or name=='D_A_T'or name=='D_B_T':
                 if isinstance(name, str):
                     load_filename = '%s_net_%s.pth' % (epoch, name)
                     load_path = os.path.join(self.save_dir, load_filename)
@@ -260,7 +261,6 @@ class BaseModel(ABC):
                     for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
                         self.__patch_instance_norm_state_dict(state_dict, net, key.split('.'))
                     net.load_state_dict(state_dict)
-        '''
 
     def print_networks(self, verbose):
         """Print the total number of parameters in the network and (if verbose) network architecture
